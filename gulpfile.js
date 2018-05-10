11038991cd718b820d70cd9cfc83b3731b72f667
dimas prasetyo <dimasprasetyo485@gmail.com>
2018-05-11 06:11:16 +0700

    update task function gulp js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,24 @@
-const gulp     = require('gulp'),
-	  sass     = require('gulp-sass'),
-	  plumber  = require('gulp-plumber'),
-	  uglify   = require('gulp-uglify');
+const gulp     	  = require('gulp'),
+	  sass    	  = require('gulp-sass'),
+	  cleanCSS    = require('gulp-clean-css');
+	  plumber  	  = require('gulp-plumber'),
+	  uglify   	  = require('gulp-uglify'),
+	  concat	  = require('gulp-concat'),
+	  pipe		  = require('gulp-pipe'), 
+	  browserSync = require('browser-sync').create();
+
+//vendor task copy third party libraries from /node_modules into /vendor
+gulp.task('vendor',function(){
+  console.log('function was working !');
+  gulp.src(['./node_modules/bootstrap/dist/**/*', ]).pipe(gulp.dest('./vendor/bootstrap'));
+  gulp.src(['./node_modules/jquery/dist/**/*', ]).pipe(gulp.dest('./vendor/jquery'));
+  gulp.src(['./node_modules/animate.css/**/*', ]).pipe(gulp.dest('./vendor/animate'));
+  gulp.src(['./node_modules/gsap/src/minified/**/*/',]).pipe(gulp.dest('./vendor/gsap'));
+});
+
 
 //Function Scripts task compressed !
-gulp.task('compress', function() {
+gulp.task('js:compress', function() {
 	// Console.log('task runner working todo :) !!');
 	function handleError(name){
 		return function (error){
@@ -21,19 +35,58 @@ gulp.task('compress', function() {
 	});
 });
 
-//Function Style's Compressed !
-gulp.task('compres-styles', function(){
+//Function Compiled CSS !
+gulp.task('css:compile', function(){
 	console.log('run stlyes was here !!');
-
+	return gulp.src('./scss/**/*.scss')
+    .pipe(sass.sync({
+      	outputStyle: 'expanded'
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('./frontend/dist/css'));
 });
+
+// Minify CSS !
+gulp.task('css:minify', ['css:compile'], function() {
+  return gulp.src([
+      './frontend/dist/css/*.css',
+      '!./frontend/dist/css/*.min.css'
+    ])
+    .pipe(cleanCSS())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(browserSync.stream());
+});
+
 
 //Function Watching JS 
 gulp.task('watch-js',function(){
 	console.log('watching files JS !!');
 	gulp.watch('dist/js');
+
+
 });
 
 //Function Watching JS 
 gulp.task('watch-style',function(){
 	console.log('watching files Style !!');
+
+
+});
+
+gulp.task('browsersync',function(){
+	console.log('its works !');
+	browserSync.init({
+		server:{
+			baseDir: "./",
+			directory:false
+		}
+	});
+});
+
+gulp.task('development',['browsersync'], function(){
+	gulp.watch('./*.php',browserSync.reload);
+  	gulp.watch('./main/*.js', ['js'], ['jshint']);
+  	// gulp.watch('./main/*.js', ['js'], ['jshint']);
 });
