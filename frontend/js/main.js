171e9eaa5053d1622397b7961b279f9c55226388
dimas prasetyo <dimasprasetyo485@gmail.com>
2018-05-29 20:30:27 +0700

    update all resource package for kinema gulp utils

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,47 +1,47 @@
 //Function Css Circle Menu
-(function(window) {
-  window.cssCircleMenu = function(el) {
-    var $menu = document.querySelector(el);
-    var $menuToggle = $menu ? $menu.querySelector('.js-menu-toggle') : undefined;
-    var $menuMask = $menu ? $menu.querySelector('.js-menu-mask') : undefined;
+// (function(window) {
+//   window.cssCircleMenu = function(el) {
+//     var $menu = document.querySelector(el);
+//     var $menuToggle = $menu ? $menu.querySelector('.js-menu-toggle') : undefined;
+//     var $menuMask = $menu ? $menu.querySelector('.js-menu-mask') : undefined;
 
-    if (!$menu || !$menuToggle || !$menuMask) {
-      throw new Error('Invalid elements, check the structure.');
-    } else {
-      init();
-    }
+//     if (!$menu || !$menuToggle || !$menuMask) {
+//       throw new Error('Invalid elements, check the structure.');
+//     } else {
+//       init();
+//     }
 
-    return {
-      openMenu: openMenu,
-      closeMenu: closeMenu
-    };
+//     return {
+//       openMenu: openMenu,
+//       closeMenu: closeMenu
+//     };
 
-    function init() {
-      $menuToggle.addEventListener('click', function(e) {
-        e.preventDefault();
-        toggleMenu();
-      });
-    }
+//     function init() {
+//       $menuToggle.addEventListener('click', function(e) {
+//         e.preventDefault();
+//         toggleMenu();
+//       });
+//     }
 
-    function toggleMenu() {
-      $menuToggle.classList.contains('is-active')
-        ? closeMenu()
-        : openMenu();
-    }
+//     function toggleMenu() {
+//       $menuToggle.classList.contains('is-active')
+//         ? closeMenu()
+//         : openMenu();
+//     }
 
-    function openMenu() {
-      $menu.classList.add('is-active');
-      $menuToggle.classList.add('is-active');
-      $menuMask.classList.add('is-active');
-    }
+//     function openMenu() {
+//       $menu.classList.add('is-active');
+//       $menuToggle.classList.add('is-active');
+//       $menuMask.classList.add('is-active');
+//     }
 
-    function closeMenu() {
-      $menu.classList.remove('is-active');
-      $menuToggle.classList.remove('is-active');
-      $menuMask.classList.remove('is-active');
-    }
-  };
-})(window);
+//     function closeMenu() {
+//       $menu.classList.remove('is-active');
+//       $menuToggle.classList.remove('is-active');
+//       $menuMask.classList.remove('is-active');
+//     }
+//   };
+// })(window);
 
 //Function Owl Slider
 $(function SliderOwl(){
